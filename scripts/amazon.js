8c2dcf949138f2932c2ee5d37a315621855c60a6
Keerthi <keerthiravi304@gmail.com>
2024-05-21 15:55:26 -0400

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -79,7 +79,12 @@ document.querySelector('.js-products-grid').innerHTML=productsHTML;
     quantity : 1
    });
    }
-   console.log(cart);
+    let cartQuantity=0;
+   cart.forEach((item) =>{
+cartQuantity += item.quantity;
+   }) ;
+
+   document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
   });
  });
  
