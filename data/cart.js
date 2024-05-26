7342a4d9cdbf996466739e6c9b1a2daf1ce06a12
Keerthi <keerthiravi304@gmail.com>
2024-05-26 18:41:32 -0400

    Save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,7 @@
-export let cart =[{
+export let cart =JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+ cart =
+ [{
   productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6' ,
   quantity:2
 },
@@ -7,6 +10,12 @@ export let cart =[{
   quantity: 1
 }
 ];
+}
+
+
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 export function addToCart(productId){
 
@@ -25,6 +34,8 @@ export function addToCart(productId){
    quantity : 1
   });
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId){
@@ -36,4 +47,5 @@ export function removeFromCart(productId){
     }
   });
   cart=newCart;
+  saveToStorage();
 }
