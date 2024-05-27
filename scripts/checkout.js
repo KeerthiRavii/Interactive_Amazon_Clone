0dabccadf7daf2a91cdf1502953ee63c7a2a8209
Keerthi <keerthiravi304@gmail.com>
2024-05-27 14:28:48 -0400

    import calculateCartQuantity from cart.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart,removeFromCart} from '../data/cart.js';
+import {cart,removeFromCart,calculateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency} from './utils/money.js';
 
@@ -120,11 +120,7 @@ cart.forEach((cartItem)=>{
 document.querySelector('.js-return-to-home-link').innerHTML=`${cartQuantity} items`; */
 
 function updateCartQuantity(){
-  let cartQuantity=0;
-  cart.forEach((cartItem) =>{
-    cartQuantity += cartItem.quantity;
-  });
-
+  const cartQuantity=calculateCartQuantity();
   document.querySelector('.js-return-to-home-link').innerHTML=`${cartQuantity} items`;
 }
 
